cce558f72a4d32f0aaced395be0ff3c90e33b22f
z-hans <38305784+z-hans@users.noreply.github.com>
2019-08-07 16:45:47 +0800

    Add files via upload

--- study/methodAndFunction.go
+++ study/methodAndFunction.go
@@ -2,20 +2,21 @@ package main
 
 import "fmt"
 
+// Employee 员工编号
 type Employee struct {
 	name string
-	age int
+	age  int
 }
 
 /*
 	这两个[方法]都有接收者, 一个接收者是一个类型, 一个接收者是一个指针类型
 
 	可以看到函数和方法的区别, 就是一个有接收者一个没有接收者, 指定了接收者
-	之后, 调用方法的时候必须要 接收者.func() 的形式来进行函数的调用, 函数
-	则可以 直接调用就行, 如果没有参数连括号都不用加上去 直接 func 就行, 
-	有函数就func(args) 如此进行调用
+	之后, 调用方法的时候必须要 接收者.func() 的形式来进行函数的调用
 
-	 
+	函数只要访问权限足够，直接调用就行func(args)
+	函数可以不加括号使用， 返回的是函数的引用地址，方法不行
+
 	而指针接收者是不是复制一份值过去, 而是将this, 将自己本体作为接收者, 方
 	法可以用本体中的一些东西, 改变也是改变本体中的东西
 
@@ -48,6 +49,10 @@ func (e *Employee) changeAge(newAge int) {
 	e.age = newAge
 }
 
+func getFixName() string {
+	return "zhs"
+}
+
 func changeName(newName string) {
 
 }
@@ -56,28 +61,23 @@ func changeAge(newAge int) {
 
 }
 
-func main () {
+func main() {
 	e := Employee{
-		name : "zhans",
-		age : 50,
+		name: "zhans",
+		age:  50,
 	}
 
-
-
-	fmt.Println("name before change: %s", e.name)
+	fmt.Println("name before change: ", e.name)
 	e.changeName("zhanghansen")
-	fmt.Println("name after change: %s", e.name)
+	fmt.Println("name after change: ", e.name)
 
-
-	fmt.Println("age before change: %s", e.age)
+	fmt.Println("age before change: ", e.age)
 	e.changeAge(20)
-	fmt.Println("age after change: %s", e.age)
+	fmt.Println("age after change: ", e.age)
 
 	// -----------------------------------
 	bytes := byte(3)
 
-	fmt.Println("bytes: %v", bytes)
-
-
+	fmt.Println("bytes: ", bytes)
 
 }
